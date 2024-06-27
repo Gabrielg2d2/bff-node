5cc8aa9248a5327ae0d3f71f95e2c2b638141f2e
Gabriel <gabriel.brito@acqio.com.br>
2024-06-27 19:10:01 -0300

    chore: config scripts

---
+++ src/index.js
@@ -0,0 +1,29 @@
+const fastify = require("fastify");
+const { PostController } = require("./controllers/posts");
+
+const app = fastify({
+    logger: true,
+});
+
+app.get("/posts", async (request, response) => {
+    const postController = new PostController();
+    const result = await postController.getPosts();
+    return response.send(result);
+});
+
+app.get("/posts/:id", async (request, response) => {
+    const postController = new PostController();
+    const result = await postController.getPostById(request.params.id);
+    return response.send(result);
+});
+
+app.listen(
+    {
+        port: 3333,
+    },
+    (error) => {
+        if (error) throw error;
+
+        console.log("Server is running on port 3333");
+    }
+);
